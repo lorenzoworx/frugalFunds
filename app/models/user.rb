e4e347b2953316e45eda696c62753e977c8c8287
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-25 10:44:12 +0100

    ADD validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_many :product, foreign_key: 'author_id'
-  has_many :group, foreign_key: 'author_id'
+  has_many :products, foreign_key: 'author_id'
+  has_many :groups, foreign_key: 'author_id'
+  validates :name, presence: true
 end
