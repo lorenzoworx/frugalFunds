42e29267f0c4303e1d83b6be502d692cb6ff117a
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-25 01:52:21 +0100

    IMPLEMENT entity relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,3 @@
 class Group < ApplicationRecord
+  belongs_to :author
 end
--- db/migrate/20230925004758_create_products.rb
+++ db/migrate/20230925004758_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.references :author, null: false, foreign_key: { to_table: :users}
       t.timestamps
     end
   end
