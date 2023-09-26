765b6d652252f0d66e8950a88fa392cca26fe824
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-26 13:32:21 +0100

    GENERATE products controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root "groups#index"
   resources :groups, only: [:show, :new, :create]
+  resources :products, only: [:new, :create]
   
   # Use landing page as root
   # get 'home/index'
