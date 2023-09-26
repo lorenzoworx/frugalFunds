13a46b596e367128114e104a182e3dd89e76bd05
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-26 14:19:40 +0100

    ADD `create` method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,21 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @group = current_user.groups.find(params[:group_id])
+    @product = current_user.products.new(product_params)
+    @product.groups << @group
+    if @product.save
+      redirect_to group_path(@group)
+    else
+      render :new      
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :amount)
+  end
+
 end
