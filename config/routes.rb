999c10500f24ee789252fbb4c09f811ba05ef14b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-26 09:49:54 +0100

    GENERATE groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
+  resources :groups, only: [:show, :new, :create]
+  
+  # Use landing page as root
+  # get 'home/index'
+  # root 'home#index' 
+  
 end
