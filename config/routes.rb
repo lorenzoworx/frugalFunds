d70c0d8ec830e67a1956cfac91d25bda96e4ce5b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-27 02:01:32 +0100

    ADD splash screen as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "groups#index"
-  resources :groups, only: [:show, :new, :create] do
+  root "splash#index"
+  resources :groups, only: [:index, :show, :new, :create] do
     resources :products, only: [:new, :create]    
   end
   
-  # Use landing page as root
-  # get 'home/index'
-  # root 'home#index' 
+ 
   
 end
