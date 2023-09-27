76bc736e0b29fec596d15e7d4ceac1c6224f4000
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-27 09:42:00 +0100

    REDIRECT on successful user login

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,4 +1,8 @@
 class SplashController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
-  def index; end
+  def index
+    if user_signed_in?
+      redirect_to groups_path
+    end
+  end
 end
