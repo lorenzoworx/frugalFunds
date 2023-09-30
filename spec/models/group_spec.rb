b645b23d9b8d41b607485a8e83c217eaaafb5358
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-30 13:26:58 +0100

    ADD necessary tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Group, type: :model do
   @test_user = User.create(name: 'John', email: 'admin@test.com', password: 'test12')
-  @test_group = Group.new(author: @test_user, name: 'test product', icon: 'https://placehold.co/100x100')
+  subject { Group.new(author: @test_user, name: 'test product', icon: 'https://placehold.co/100x100') }
 
-  before { @test_group.save }
+  before { subject.save }
   it 'Should have a name present' do
-    @test_group.name = nil
-    expect(@test_group).to_not be_valid
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 
   it 'Should have an icon present' do
-    @test_group.icon = nil
-    expect(@test_group).to_not be_valid
+    subject.icon = nil
+    expect(subject).to_not be_valid
   end
 end
