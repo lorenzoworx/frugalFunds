3bc7df7bfa3d436a3deabde55ff132ed366be5ef
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-30 02:15:10 +0100

    ADD splash screen test

---
+++ spec/views/splash/index.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Spalsh/Index.', type: :system do
+  describe 'splash index page' do
+    before do
+      visit root_path
+    end
+    it 'Has app name' do
+      expect(page).to have_content('Frugal Funds')
+    end
+    it 'Redirects to the log in page when the login button is clicked' do
+      click_link 'Log in'
+      expect(page).to have_current_path(new_user_session_path)
+    end
+    it 'Redirects to the sign up page when the login button is clicked' do
+      click_link 'Sign up'
+      expect(page).to have_current_path(new_user_registration_path)
+    end
+  end
+end
